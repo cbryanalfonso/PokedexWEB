deaba265dba8c91ea84cf437ecc46352e2ad950a
cbryanalfonso <cbryanalfonso@gmail.com>
2022-03-17 08:23:40 -0600

    Busqueda Pokemones

--- js/main.js
+++ js/main.js
@@ -17,9 +17,9 @@ const fetchPokemon = () => {
         .then((value) => {
             if (value) {
                 console.log(value)
-                let pokeImg = value.sprites.front_default;
-                pokeImage(pokeImg)
-                console.log(pokeImg)
+                //let pokeImg = value.sprites.front_default;
+                pokeImage(value)
+                console.log(value)
                 //nombrePokemon(value);
             }
         });
@@ -28,12 +28,16 @@ const fetchPokemon = () => {
 
 const pokeImage = (url) => {
     const pokePhoto = document.getElementById("pokeImg");
-    document.getElementById("nombrePoke").innerHTML = url;
+    document.getElementById("nombrePoke").innerHTML = url.forms[0].name;
+    document.getElementById("att").innerHTML= url.stats[1].base_stat;
+    document.getElementById("def").innerHTML = url.stats[2].base_stat;
+    document.getElementById("tipo").innerHTML = url.types[0].type.name
     // var btn = document.createElement("P");
     // nombrePoke.
     //btn.innerHTML= nombrePoke
     // nombrePoke.innerHTML(url)
-    pokePhoto.src = url;
+    pokePhoto.src = url.sprites.front_default;
+
 }
 function nombrePokemon(value) {
     console.log(value);
