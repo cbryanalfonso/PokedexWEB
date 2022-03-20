61b024efcd16c8934afd031938e3678d64b80b11
cbryanalfonso <cbryanalfonso@gmail.com>
2022-03-19 18:54:53 -0600

    PokeWEB

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
+let arregloAbilities = [];
+let arregloMovimientos = []
 const fetchPokemon = () => {
+    
     const pokeNameInput = document.getElementById("inputSearch");
     let pokeName = pokeNameInput.value;
     pokeName = pokeName.toLowerCase();
@@ -8,6 +11,7 @@ const fetchPokemon = () => {
         .then((value) => {
             if (value.status != "200") {
                 console.log("Error -> ", res);
+
             } else {
                 return value.json()
             }
@@ -19,8 +23,22 @@ const fetchPokemon = () => {
                 console.log(value)
                 //let pokeImg = value.sprites.front_default;
                 pokeImage(value)
-                console.log(value)
+                console.log(value.abilities)
                 //nombrePokemon(value);
+               /*  arregloAbilities.push(value.abilities.length) */
+                const arregoHabilidades = (value.abilities)
+                arregoHabilidades.map((i)=>{
+                   // console.log(i.ability.name);
+                    arregloAbilities.push(i.ability.name)
+                    arregloAbilities.join('<br>')
+                })
+                const arregloMoves = (value.moves)
+                arregloMoves.map((i)=>{
+                    arregloMovimientos.push(i.move.name)
+                    console.log(i.move.name);
+                })
+                 console.log("Las habilidades" + arregloAbilities);
+                 habilidadesPokemon(arregloAbilities)
             }
         });
 }
@@ -32,13 +50,18 @@ const pokeImage = (url) => {
     document.getElementById("att").innerHTML= url.stats[1].base_stat;
     document.getElementById("def").innerHTML = url.stats[2].base_stat;
     document.getElementById("tipo").innerHTML = url.types[0].type.name
+    //document.getElementById("pokeImgModal").innerHTML = url.sprites.front_default;
+    const pokePhotoModal = document.getElementById("pokeImgModal");
     // var btn = document.createElement("P");
     // nombrePoke.
     //btn.innerHTML= nombrePoke
     // nombrePoke.innerHTML(url)
     pokePhoto.src = url.sprites.front_default;
+    pokePhotoModal.src = url.sprites.front_default; 
 
 }
-function nombrePokemon(value) {
-    console.log(value);
+function habilidadesPokemon(value) {
+    console.log("Funcion nueva ...",arregloAbilities);
+    document.getElementById("abilityPoke").innerHTML = arregloAbilities 
+    document.getElementById("movePoke").innerHTML = arregloMovimientos
 }
